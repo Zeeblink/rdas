6046dde15e5e4de45cabd0af0780e02447fe46f5
Zeeblink <eabiodun45@gmail.com>
2024-10-30 13:32:54 +0100

    ...

--- app/components/AboutDropdown.tsx
+++ app/components/AboutDropdown.tsx
@@ -17,17 +17,13 @@ const AboutDropdown: React.FC<AboutDropdownProps> = ({ isOpen }) => {
         className="block px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-secondary transition duration-300">
         What We Believe In
       </Link>
-      <Link href="/newsroom"
-        className="block px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-secondary transition duration-300">
-        Newsroom
-      </Link>
       <Link href="/careers"
         className="block px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-secondary transition duration-300">
         Careers
       </Link>
       <Link href="/investors"
         className="block px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-secondary transition duration-300">
-        Investorss
+        Investors
       </Link>
     </div>
   );
