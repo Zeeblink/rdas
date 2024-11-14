d268398648e6ce4165f7d039e610df224d246c11
Zeeblink <eabiodun45@gmail.com>
2024-11-14 21:30:45 +0100

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://rdas.com.ng',
+      url: 'https://www.rdas.com.ng',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://rdas.com.ng/about',
+      url: 'https://www.rdas.com.ng/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://acme.com/contact',
+      url: 'https://www.rdas.com.ng/contact',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
